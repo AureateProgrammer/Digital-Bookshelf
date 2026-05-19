5e4cd5caf45533fbf33dc76764c0e9ab4cea1e6b
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-05-19 15:45:29 -0400

    task 3 creating schema

---
+++ models/book.js
@@ -0,0 +1,11 @@
+    const bookSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    isbn: String,
+    publishDate: Date,
+    inStock: Boolean
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
